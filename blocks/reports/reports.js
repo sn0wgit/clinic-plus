8434b5af99c0f11a44d3a15f225fac293741e8c0
Ramil <ramil26102006@gmail.com>
2024-08-12 18:15:09 +0300

    mobile adaptive update

--- blocks/reports/reports.js
+++ blocks/reports/reports.js
@@ -2,5 +2,9 @@ export const reports = () => {
   new Swiper(".reports__slider", {
     slidesPerView: 2,
     spaceBetween: 50,
+    navigation: {
+      nextEl: ".reports__slider-button.next",
+      prevEl: ".reports__slider-button.prev",
+    },
   });
 };
