b7eb2e5d1739b9c19be4b2638decaecd81dd4dfd
Ramil <ramil26102006@gmail.com>
2024-08-18 00:44:12 +0300

    preloader fixed

--- components/preloader/preloader.js
+++ components/preloader/preloader.js
@@ -1,22 +1,13 @@
 export const preloader = () => {
-  const preloaderDiv = document.createElement("div");
-  preloaderDiv.className = "preloader fullscreen";
-  preloaderDiv.innerHTML = `
-      <div class="preloader__body container">
-       <div class="pill">
-		<div class="pill__main">
-		 <div class="pill__glare"></div>
-		</div>
-		<div class="pill__shadow"></div>
-	   </div>
-      </div>
-  `;
+    const preloaderDiv = document.querySelector(".preloader");
+    
+    preloaderDiv.style.display = "flex";
+    document.body.style.overflow = "hidden";
 
-  document.body.prepend(preloaderDiv);
-
-  setTimeout(() => {
-      preloaderDiv.remove();
-  }, 3000);
+    setTimeout(() => {
+        preloaderDiv.style.display = "none";
+        document.body.style.overflow = "auto";
+    }, 3000);
 };
 
 window.addEventListener("load", preloader);
